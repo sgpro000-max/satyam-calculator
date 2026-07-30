ae7cc465ace2295bc525fab7b54bf4553d2ff572
sgpro000-max <sgpro000@gmail.com>
2026-07-30 14:46:42 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,20 @@
-# Calculator Engine
+def press(expression, value):
+    return expression + str(value)
+
+
+def clear():
+    return ""
+
+
+def backspace(expression):
+    return expression[:-1]
+
+
+def calculate(expression):
+    try:
+        expression = expression.replace("×", "*")
+        expression = expression.replace("÷", "/")
+        result = str(eval(expression))
+        return result
+    except:
+        return "Error"
