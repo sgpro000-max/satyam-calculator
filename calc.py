751cf17d954a7e7a2bcf19a3caa0715d3e04e33d
sgpro000-max <sgpro000@gmail.com>
2026-07-30 13:28:39 +0530

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.title("🧮 SATYAM CALCULATOR")
+
+num1 = st.number_input("Pahli Rashi", value=0.0)
+op = st.selectbox("Operator", ["+", "-", "*", "/"])
+num2 = st.number_input("Dushri Rashi", value=0.0)
+
+if st.button("Calculate"):
+    if op == "+":
+        st.success(num1 + num2)
+    elif op == "-":
+        st.success(num1 - num2)
+    elif op == "*":
+        st.success(num1 * num2)
+    elif op == "/":
+        if num2 != 0:
+            st.success(num1 / num2)
+        else:
+            st.error("Zero se Divide Nahi Hota!")
