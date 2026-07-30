34af28514d3c79a2e6a8d456fa2baa6e3ae0afae
sgpro000-max <sgpro000@gmail.com>
2026-07-30 16:30:40 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -11,10 +11,23 @@ def backspace(expression):
 
 
 def calculate(expression):
+    if not expression:
+        return ""
+
     try:
-        expression = expression.replace("×", "*")
-        expression = expression.replace("÷", "/")
-        result = str(eval(expression))
-        return result
-    except:
+        expression = (
+            expression
+            .replace("×", "*")
+            .replace("÷", "/")
+            .replace("%", "/100")
+        )
+
+        result = eval(expression)
+
+        if isinstance(result, float) and result.is_integer():
+            result = int(result)
+
+        return str(result)
+
+    except Exception:
         return "Error"
